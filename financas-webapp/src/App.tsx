3f64d8d06e97f1019eed1269dd01d6900fde088e
IslanP1 <islanpereiradeoliveira@gmail.com>
2026-06-14 23:11:50 -0300

    feat: adicionar componente ProtectedRoute para gerenciar acesso às rotas

--- financas-webapp/src/App.tsx
+++ financas-webapp/src/App.tsx
@@ -2,14 +2,17 @@ import { Routes, Route } from 'react-router-dom';
 import Register from './pages/Register/Register';
 import Login from './pages/Login/Login';
 import Home from './pages/Home/Home';
-
+import ProtectedRoute from './components/ProtectedRoute';
 
 export default function App() {
     return (
         <Routes>
-            <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+
+            <Route element={<ProtectedRoute />}>
+                <Route path="/" element={<Home />} />
+            </Route>
         </Routes>
     );
 }
